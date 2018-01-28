9dc3098a9cf8be917ed283602aa44a5c5405a0e9
Nora Galvin <minhnora98@gmail.com>
2018-01-28 16:07:24 +0700

    fix something

--- upload.php
+++ upload.php
@@ -3,21 +3,21 @@
     // print_r($_POST);
         if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-            if (!file_exists('images')) {
-                mkdir('images', 0777, true);
+            if (!file_exists('upload')) {
+                mkdir('upload', 0777, true);
                 // Upload file
                 $temp = explode(".", $_FILES["image"]["name"]);
                 $newfilename = round(microtime(true)) . '.' . end($temp);
-                move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $newfilename);
+                move_uploaded_file($_FILES['image']['tmp_name'], './upload/' . $newfilename);
                 // print_r($_SERVER);
-                echo "images/" .  $newfilename;
+                echo "upload/" .  $newfilename;
             }else{
                 // Upload file
                 $temp = explode(".", $_FILES["image"]["name"]);
                 $newfilename = round(microtime(true)) . '.' . end($temp);
-                move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $newfilename);
+                move_uploaded_file($_FILES['image']['tmp_name'], './upload/' . $newfilename);
                 // print_r($_SERVER);
-                echo "images/" .  $newfilename;
+                echo "upload/" .  $newfilename;
             }
         }
 ?>
