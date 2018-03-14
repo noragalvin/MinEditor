69d64ab458ffb8ae4cf63939c362bb21336cf99f
Nora Galvin <minhnora98@gmail.com>
2018-03-14 15:45:17 +0700

    up

--- assets/js/keetool-editor.js
+++ assets/js/keetool-editor.js
@@ -1,6 +1,6 @@
 function init(id) {
     content = `
-<link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <style>
     .k-caret {
